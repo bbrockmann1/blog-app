1052a720aae420aa6fd88b6ae8f61f0ac7613af1
Bo Brockmann <bbrockmann1@gmail.com>
2022-12-20 16:28:16 -0700

    link routes to navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Menu, Segment } from 'semantic-ui-react'
+import { Link  } from 'react-router-dom'
 
 export default class Navbar extends Component {
   state = { activeItem: 'home' }
@@ -16,27 +17,37 @@ export default class Navbar extends Component {
             name='home'
             active={activeItem === 'home'}
             onClick={this.handleItemClick}
+            as={Link}
+            to={'/'}
           />
           <Menu.Item
             name='blogs'
             active={activeItem === 'blogs'}
             onClick={this.handleItemClick}
+            as={Link}
+            to={'/userblog'}
           />
           <Menu.Item
             name='make a new blog'
             active={activeItem === 'make a new blog'}
             onClick={this.handleItemClick}
+            as={Link}
+            to={'/create'}
           />
           <Menu.Item
             position='right'
             name='login'
             active={activeItem === 'login'}
             onClick={this.handleItemClick}
+            as={Link}
+            to={'/login'}
           />
           <Menu.Item
             name='logout'
             active={activeItem === 'logout'}
             onClick={this.handleItemClick}
+            as={Link}
+            to={'/'}
           />
         </Menu>
       </Segment>
