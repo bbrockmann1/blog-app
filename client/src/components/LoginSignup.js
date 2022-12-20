c7100f6cef031bd4a8db06232c0a168fd2886d56
Bo Brockmann <bbrockmann1@gmail.com>
2022-12-20 14:05:01 -0700

    added wireframe components

---
+++ client/src/components/LoginSignup.js
@@ -0,0 +1,15 @@
+import Login from "./Login";
+import Signup from "./Signup";
+
+function LoginSignup() {
+
+    return (
+      <>
+        <Login />
+        <Signup />
+      </>
+    );
+  }
+  
+  export default LoginSignup;
+  
