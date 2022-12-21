9e4544814096d5197b0cf97145f11b10139ab891
Bo Brockmann <bbrockmann1@gmail.com>
2022-12-21 10:50:20 -0700

    innit

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,22 @@
+import { Button, Form, Message } from "semantic-ui-react";
+
 function Login() {
 
     return (
       <>
-        <h1>Login</h1>
+        <div className="auth-main">
+        <div class="auth-content">
+          <div className="auth-card">
+            <img src={null} alt="Logo" className="auth-logo" />
+            <Header as="h2" color="black" textAlign="center">
+              {null}
+            </Header>
+            <Form.Group size="large" className="auth-form" autocomplete="off">
+              {null}
+            </Form.Group>
+          </div>
+        </div>
+      </div>
       </>
     );
   }
