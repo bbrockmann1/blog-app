8d7ca8c2c9dcb2c0c81488246e089a086985fa28
Robert Brockmann <bbrockmann1@gmail.com>
2022-12-23 10:10:58 -0700

    innit

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,9 +1,25 @@
+import { Card } from 'semantic-ui-react'
+import { useEffect, useState } from 'react';
+
 function Homepage() {
+const [ blogs, setBlogs ] = useState([])
+
+useEffect(() => {
+  fetch('/blogs')
+  .then(resp => resp.json())
+  .then(blogsArray => {
+    setBlogs(blogsArray)
+  })
+}, [])
 
     return (
-      <>
-        <h1>HomePage</h1>
-      </>
+  <Card.Group>
+    <Card fluid color='red' header={'Option 1'} />
+    <Card fluid color='red' header={'Option 2'} />
+    <Card fluid color='red' header={'Option 3'} />
+    <Card fluid color='red' header={'Option 4'} />
+    <Card fluid color='red' header={'Option 5'} />
+  </Card.Group>
     );
   }
   
