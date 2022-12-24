8efd8c18f46b86441e659a71b9a8498441c94b3d
Robert Brockmann <bbrockmann1@gmail.com>
2022-12-23 20:34:16 -0700

    npm confetti package

--- client/src/components/auth/Done.js
+++ client/src/components/auth/Done.js
@@ -1,8 +1,18 @@
-import React, { Component } from "react";
+import Confetti from 'react-confetti'
+import { Message } from 'semantic-ui-react'
 
-class Done extends Component {
-  render() {
-    return <div>Thanks for signing up!</div>;
-  }
+function Done() {
+  return(
+    <>
+      <Confetti />
+      <Message>
+        <Message.Header>Thank you for signing up!</Message.Header>
+          <Message.List>
+            <Message.Item>Navigate though the navbar to create a blog!</Message.Item>
+            <Message.Item>Or click the logout button to logout.</Message.Item>
+          </Message.List>
+      </Message>
+    </>
+  )
 }
 export default Done;
