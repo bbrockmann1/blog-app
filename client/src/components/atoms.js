8d6446972f5f0eae232e74716835dd99bddaead8
Bo Brockmann <bbrockmann1@gmail.com>
2022-12-31 21:44:22 -0700

    added erros and signup state

--- client/src/components/atoms.js
+++ client/src/components/atoms.js
@@ -1,6 +1,17 @@
 import { atom } from 'recoil'
 
 export const blogsAtom = atom({
-    key: 'blogsAtom',
+    key: 'blogs',
     default: []
 });
+
+export const signupAtom = atom({
+    key: 'signup',
+    default: false
+});
+
+export const errorsAtom = atom({
+    key: 'errors',
+    default: []
+});
+
