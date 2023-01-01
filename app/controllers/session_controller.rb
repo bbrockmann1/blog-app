5bf748bf81b0be722fbf2ef477a6108827f6d249
Robert Brockmann <bbrockmann1@gmail.com>
2022-12-31 19:12:57 -0700

    update

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+    def create
+        user = User.find_by(email: params[:email])
+        if user&.authenticate(params[:password])
+            session[user_id] = user.id
+            render json: user, status: :ok
+        else
+            render json: {errors: 'Incorrect email or password.'}
+        end
+    end
+
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
+end
