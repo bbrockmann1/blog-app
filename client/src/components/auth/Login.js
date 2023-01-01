575fce26abde329605fb1183e38a89ce4ad2c519
Bo Brockmann <bbrockmann1@gmail.com>
2022-12-31 21:52:20 -0700

    update

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,41 +1,43 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { Button, Form, Message } from "semantic-ui-react";
 import Layout from "./Layout";
+import { useRecoilState } from 'recoil';
+import { signupAtom, errorsAtom } from './atoms'
 
-class Login extends Component {
+function Login() {
+  const [signup, setSignup] = useRecoilState(signupAtom); 
+  const [errors, setErrors] = useRecoilState(errorsAtom);
+  
+  return (
+    <Layout header="Log in">
+      <Form.Input
+        fluid
+        icon="user"
+        iconPosition="left"
+        placeholder="E-mail address"
+        className="auth-input-field"
+      />
+      <Form.Input
+        fluid
+        icon="lock"
+        iconPosition="left"
+        placeholder="Password"
+        type="password"
+        className="auth-input-field"
+      />
 
-  render() {
+      <Link to="/blogs">
+        <Button color="teal" fluid size="huge">
+          Login
+        </Button>
+      </Link>
 
-    return (
-      <Layout header="Log in" >
-        <Form.Input
-          fluid
-          icon="user"
-          iconPosition="left"
-          placeholder="E-mail address"
-          className="auth-input-field"
-        />
-        <Form.Input
-          fluid
-          icon="lock"
-          iconPosition="left"
-          placeholder="Password"
-          type="password"
-          className="auth-input-field"
-        />
+      <Message size="big">
+        <Link to="/signup">Not Registered?</Link>
+      </Message>
+    </Layout>
+  );
+};
 
-        <Link to="/blogs" >
-          <Button color="teal" fluid size="huge" >
-            Login
-          </Button>
-        </Link>
-
-        <Message size="big">
-          <Link to="/signup">Not Registered?</Link>
-        </Message>
-      </Layout>
-    );
-  }
-}
 export default Login;
