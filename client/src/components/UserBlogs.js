1cc4fdb94a77428a3718d84f1c605495afff2720
Bo Brockmann <bbrockmann1@gmail.com>
2023-01-02 09:09:04 -0700

    update

--- client/src/components/UserBlogs.js
+++ client/src/components/UserBlogs.js
@@ -1,10 +1,14 @@
 import { Card } from 'semantic-ui-react';
+import { useRecoilValue } from 'recoil'
+import { currentUserAtom } from './atoms.js'
 
 function UserBlogs() {
-  
+    const currentUser = useRecoilValue(currentUserAtom)
+
     return (
       <Card.Group>
-        {/* {userBlogCards} */}
+        <h1>{currentUser.email}</h1>
+        <h1>{currentUser.first_name}</h1>
       </Card.Group>
     );
   }
