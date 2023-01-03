e153e827980d896977f5a06abb402bc87a1b5350
Bo Brockmann <bbrockmann1@gmail.com>
2023-01-03 09:24:37 -0700

    blog form v1

--- client/src/components/BlogForm.js
+++ client/src/components/BlogForm.js
@@ -1,22 +1,74 @@
-import { Form } from "semantic-ui-react";
+import { Form, FormField, Button } from "semantic-ui-react";
 import { currentUserAtom } from './atoms';
 import { useRecoilValue } from 'recoil';
+import { useState } from 'react';
 
 function BlogForm() {
-  const currentUser = useRecoilValue(currentUserAtom)
-  
-  return (
-    <Form>
-      <Form.Field>
-        <label>Title</label>
-        <input />
-      </Form.Field>
-      <Form.Field>
-        <label>Tell your story below...</label>
-        <input />
-      </Form.Field>
-    </Form>
-  );
+  const currentUser = useRecoilValue(currentUserAtom);
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [tag, setTag] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newBlog = {
+      title: title,
+      content: content,
+      tag_id: tag,
+      user_id: currentUser.id
+    };
+
+    const configObject = {
+      method: 'POST',
+      headers: {'Content-Type': 'Application/json'},
+      body: JSON.stringify(newBlog)
+    };
+
+    fetch('/blogs', configObject)
+  };
+
+    return(
+      <Form onSubmit={handleSubmit}>
+        <FormField>
+          <label>Title</label>
+          <input 
+            name="title"  
+            value={title}
+            onChange={e => setTitle(e.target.value)} 
+          />
+        </FormField>
+        <FormField>
+          <label>Tell your story below...</label>
+          <input 
+            name="content" 
+            value={content}
+            onChange={e => setContent(e.target.value)} 
+          />
+        </FormField>
+        
+      <label>
+          <strong>Add a tag...</strong>
+          <select onChange={e => setTag(e.target.value)} >
+            <option value="1">Fashion</option>
+            <option value="2">Travel</option>
+            <option value="3">Music</option>
+            <option value="4">DIY</option>
+            <option value="5">Sports</option>
+            <option value="6">Lifestyle</option>
+            <option value="7">Finance</option>
+            <option value="8">Politics</option>
+            <option value="9">Parenting</option>
+            <option value="10">Movies</option>
+            <option value="11">Tech</option>
+            <option value="12">Development</option>
+            <option value="13">Gaming</option>
+            <option value="14">Auto</option>
+          </select>
+        </label>
+        <Button type="submit">Submit</Button>
+      </Form>
+    );
   }
   
   export default BlogForm;
